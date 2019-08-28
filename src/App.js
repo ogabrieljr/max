a000c75af17a7421ead6ffe88f32db5e93dedfe9
Gabriel <jgj_junior@yahoo.com.br>
2019-08-27 23:32:35 -0300

    3.

--- src/App.js
+++ src/App.js
@@ -1,66 +1,24 @@
-import React, { useState } from "react";
-import Person from "./Person/Person";
+import React, { Component } from 'react'
+import UserInput from "./UserInput/UserInput"
+import UserOutput from "./UserOutput/UserOutput"
 
-export default function App() {
-	const [person, setPerson] = useState({
-		persons: [
-			{ name: "NAME", age: 10 },
-			{ name: "NAME2", age: 20 },
-			{ name: "NAME3", age: 30 }
-		]
-	});
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+       username:"NAME"
+    }
+  }
+  
+changeName = e => this.setState({username:e.target.value})
 
-	const [otherState] = useState("other state");
-
-	console.log(person, otherState);
-
-	const switchName = () => {
-		setPerson({
-			persons: [
-				{ name: "NAME4", age: 40 },
-				{ name: "NAME5", age: 50 },
-				{ name: "NAME6", age: 60 }
-			]
-		});
-	};
-	return (
-		<div>
-			<button onClick={switchName}>btn</button>
-			<Person name={person.persons[0].name} age={person.persons[0].age}></Person>
-			<Person name={person.persons[1].name} age={person.persons[1].age}></Person>
-			<Person name={person.persons[2].name} age={person.persons[2].age}>
-				Children
-			</Person>
-		</div>
-	);
+  render() {
+    return (
+      <div>
+        <UserInput currentName={this.state.username} changeName={this.changeName}/>
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+      </div>
+    )
+  }
 }
-
-// import React, { Component } from "react";
-// export default class App extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			count: 0
-// 		};
-// 	}
-// 	render() {
-// 		return (
-// 			<div>
-// 				<button onClick={() => this.setState({ count: this.state.count + 1 })}>
-// 					btn
-// 				</button>
-// 				{this.state.count}
-// 			</div>
-// 		);
-// 	}
-// }
-// import React, { useState } from "react";
-// export default function App() {
-// 	const [count, setCount] = useState(0);
-// 	return (
-// 		<div>
-// 			<button onClick={() => setCount(count + 1)}>btn</button>
-// 			{count}
-// 		</div>
-// 	);
-// }
