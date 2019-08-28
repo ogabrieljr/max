b245360b24fb31271cd8f04d44ce33a956b6b177
Gabriel <jgj_junior@yahoo.com.br>
2019-08-28 14:49:02 -0300

    4.4

--- src/App.js
+++ src/App.js
@@ -6,65 +6,42 @@ export default class App extends Component {
 		super(props);
 		this.state = {
 			persons: [
-				{ id: "1", name: "NAME", age: 23 },
-				{ id: "2", name: "NAME2", age: 33 },
-				{ id: "3", name: "NAME3", age: 43 }
+				{ id: "a", name: "NAME", age: 23 },
+				{ id: "b", name: "NAME2", age: 33 },
+				{ id: "c", name: "NAME3", age: 43 }
 			],
 			showPersons: false
 		};
 	}
-	tooglePerson = () => {
-		const show = this.state.showPersons;
-		this.setState({ showPersons: !show });
-	};
-
-	nameChange = (e, id) => {
-		const personIndex = this.state.persons.findIndex(person => {
-			return person.id === id;
-		});
-		const person = {
-			...this.state.persons[personIndex]
-		};
-		// const person = Object.assign({}, this.state.persons[personIndex])
-		person.name = e.target.value;
+	nameChange = (event, id) => {
+		const personIndex = this.state.persons.findIndex(person => person.id === id);
 		const persons = [...this.state.persons];
-		persons[personIndex] = person;
-		this.setState({
-			persons: persons
-		});
-	};
-
-
-	deletePerson = index => {
-		// const persons = this.state.persons.slice()
-		const persons = [...this.state.persons];
-		persons.splice(index, 1);
+		persons[personIndex].name = event.target.value;
 		this.setState({ persons });
 	};
-
-
+	deletePerson = id => {
+		const personIndex = this.state.persons.findIndex(person => person.id === id);
+		const persons = [...this.state.persons];
+		persons.splice(personIndex, 1);
+		this.setState({ persons });
+	};
+	personsToggle = () => this.setState({ showPersons: !this.state.showPersons });
 	render() {
 		let persons = null;
 		if (this.state.showPersons) {
-			persons = (
-				<div>
-					{this.state.persons.map((person, index) => {
-						return (
-							<Person
-								key={person.id}
-								deletePerson={this.deletePerson}
-								name={person.name}
-								age={person.age}
-								nameChange={e => this.nameChange(e, person.id)}
-							/>
-						);
-					})}
-				</div>
-			);
+			persons = this.state.persons.map(person => (
+				<Person
+					deletePerson={() => this.deletePerson(person.id)}
+					nameChange={e => this.nameChange(e, person.id)}
+					key={person.id}
+					name={person.name}
+					age={person.age}
+				/>
+			));
 		}
 		return (
 			<div>
-				<button onClick={this.tooglePerson}>btn</button>
+				<button onClick={this.personsToggle}>btn</button>
 				{persons}
 			</div>
 		);
